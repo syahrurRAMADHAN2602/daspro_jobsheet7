a465cce22679d0c8b80d8770170c2d617e55ac99
SyahrurRamadhan <syahrurramadhan2609@gmail.com>
2025-10-16 08:55:47 +0700

    menguubah kode siakad while

--- SiakadWhile23.java
+++ SiakadWhile23.java
@@ -22,6 +22,7 @@ public class SiakadWhile23 {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " Adalah A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " Adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
